e61e0260ce332ab781180570558860c328368bbc
michael smith and neil larion <michaelanthonysmith00@gmail.com>
2016-01-14 11:05:05 -0800

    add content for css, and event handler and scripts file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-
-
-
-
+  $("#css").click(function(){
+    if($(".css").css("display") === "none") {
+      $(".css").show();
+    }else {
+      $(".css").hide();
+    }
+  });
 });
