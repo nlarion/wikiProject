9a84420ab9c7858ad171dc9751c982495bf2a8c1
michael smith and neil larion <neil.larion@gmail.com>
2016-01-14 13:40:48 -0800

    Fishish layout and add design for each concept

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,46 @@ $(document).ready(function() {
       $(".css").hide();
     }
   });
+
+  $("#divsAndSpans").click(function(){
+    if($(".divsAndSpans").css("display") === "none") {
+      $(".divsAndSpans").show();
+    }else {
+      $(".divsAndSpans").hide();
+    }
+  });
+
+  $("#gridSystem").click(function(){
+    if($(".gridSystem").css("display") === "none") {
+      $(".gridSystem").show();
+    }else {
+      $(".gridSystem").hide();
+    }
+  });
+
+  $("#arithmetic").click(function(){
+    if($(".arithmetic").css("display") === "none") {
+      $(".arithmetic").show();
+    }else {
+      $(".arithmetic").hide();
+    }
+  });
+
+  $("#functions").click(function(){
+    if($(".functions").css("display") === "none") {
+      $(".functions").show();
+    }else {
+      $(".functions").hide();
+    }
+  });
+
+  $("#dom").click(function(){
+    if($(".dom").css("display") === "none") {
+      $(".dom").show();
+    }else {
+      $(".dom").hide();
+    }
+  });
+
+
 });
